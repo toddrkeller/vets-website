ab118793654082873731ee7181549db91418ec91
Ryan McAuliffe <ryanmcauliffe@ablevets.com>
2018-08-17 12:30:29 -0400

    Initial experiment of adding Jest into the vets-website codebase. To test yourself run yarn run v1.7.0 $ jest Done in 2.86s. from the command line.

---
+++ src/applications/facility-locator/tests/reducers/searchResult.jest.spec.js
@@ -0,0 +1,14 @@
+/* import {
+  FETCH_VA_FACILITY,
+  FETCH_VA_FACILITIES,
+  FETCH_CC_PROVIDERS,
+  SEARCH_FAILED
+} from '../../utils/actionTypes'; */
+
+describe('Reducer Tests :: SearchResultReducer', () => {
+
+  test('Fluff Test To Prove Jest Is Wired In Correctly', () => {
+    /* global expect */
+    expect(true).toEqual(true);
+  });
+});
