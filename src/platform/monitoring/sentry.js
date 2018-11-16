da4e9249aa2bf89034204a3bcf4c2056ebb32b48
Anna Carey <anna@adhocteam.us>
2018-11-16 16:29:00 -0500

    todo verify url

--- src/platform/monitoring/sentry.js
+++ src/platform/monitoring/sentry.js
@@ -2,8 +2,7 @@
  * Initializes error reporting to Sentry when running at a higher enviroment than localhost
  */
 
-import Raven from 'raven-js';
-
+import * as Sentry from '@sentry/browser';
 import environment from '../utilities/environment';
 
 // url check is necessary for e2e tests and local environments
@@ -11,7 +10,8 @@ const trackErrors = environment.BASE_URL.indexOf('localhost') < 0;
 
 if (trackErrors) {
   const url = `${environment.BASE_URL}/js-report/0`.replace('//', '//faker@');
-  Raven.config(url).install();
+
+  Sentry.init({ dsn: url });
 
   // this is for errors that happen in promises
   // it does not work locally with the webpack devtool setting we
@@ -24,9 +24,9 @@ if (trackErrors) {
     };
 
     if (evt && evt.reason) {
-      Raven.captureException(evt.reason, options);
+      Sentry.captureException(evt.reason, options);
     } else {
-      Raven.captureMessage('Unhandled promise rejection', options);
+      Sentry.captureMessage('Unhandled promise rejection', options);
     }
   });
 }
