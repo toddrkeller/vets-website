209b9dcd5a31b3f0b9890a70f85192d32ca02a83
Dan Shawkey <shawkey_daniel@bah.com>
2019-04-18 14:42:27 -0400

    add unit tests to verify noneApply behavior

--- src/applications/edu-benefits/tests/0994/pages/highTechIndustry.unit.spec.jsx
+++ src/applications/edu-benefits/tests/0994/pages/highTechIndustry.unit.spec.jsx
@@ -2,16 +2,11 @@ import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
 import sinon from 'sinon';
-// import _ from 'lodash';
+import _ from 'lodash';
 
-import {
-  DefinitionTester,
-  // getFormDOM,
-} from '../../../../../platform/testing/unit/schemaform-utils.jsx';
+import { DefinitionTester } from '../../../../../platform/testing/unit/schemaform-utils.jsx';
 
 import formConfig from '../../../0994/config/form.js';
-// import ReactTestUtils from 'react-dom/test-utils';
-// import { selectCheckbox } from '../../../../../platform/testing/unit/schemaform-utils';
 import HighTechEmploymentTypeView from '../../../../edu-benefits/0994/components/HighTechEmploymentTypeView';
 
 describe('High tech industry page', () => {
@@ -151,101 +146,95 @@ describe('High tech industry page', () => {
     form.unmount();
   });
 
-  // const highTechnologyEmploymentTypes = {
-  //   computerProgramming:
-  //     'input#computerProgramming[name="computerProgramming"]',
-  //   computerSoftware: 'input#computerSoftware[name="computerSoftware"]',
-  //   dataProcessing: 'input#dataProcessing[name="dataProcessing"]',
-  //   informationSciences:
-  //     'input#informationSciences[name="informationSciences"]',
-  //   mediaApplication: 'input#mediaApplication[name="mediaApplication"]',
-  //   noneApply: 'input#noneApply[name="noneApply"]',
-  // };
+  const highTechnologyEmploymentTypes = {
+    computerProgramming:
+      'input#computerProgramming[name="computerProgramming"]',
+    computerSoftware: 'input#computerSoftware[name="computerSoftware"]',
+    dataProcessing: 'input#dataProcessing[name="dataProcessing"]',
+    informationSciences:
+      'input#informationSciences[name="informationSciences"]',
+    mediaApplication: 'input#mediaApplication[name="mediaApplication"]',
+    noneApply: 'input#noneApply[name="noneApply"]',
+  };
 
-  // const getCheckboxes = formDOM => {
-  //   const checkboxes = {
-  //     computerProgramming: formDOM.getElement(
-  //       highTechnologyEmploymentTypes.computerProgramming,
-  //     ),
-  //     computerSoftware: formDOM.getElement(
-  //       highTechnologyEmploymentTypes.computerSoftware,
-  //     ),
-  //     dataProcessing: formDOM.getElement(
-  //       highTechnologyEmploymentTypes.dataProcessing,
-  //     ),
-  //     informationSciences: formDOM.getElement(
-  //       highTechnologyEmploymentTypes.informationSciences,
-  //     ),
-  //     mediaApplication: formDOM.getElement(
-  //       highTechnologyEmploymentTypes.mediaApplication,
-  //     ),
-  //     noneApply: formDOM.getElement(highTechnologyEmploymentTypes.noneApply),
-  //   };
-  //   return checkboxes;
-  // };
+  const getCheckboxes = form => {
+    const checkboxes = {
+      computerProgramming: _.get(
+        form,
+        'input[name*="computerProgramming"]',
+        false,
+      ),
+      computerSoftware: _.get(form, 'input[name*="computerSoftware"]', false),
+      dataProcessing: _.get(form, 'input[name*="dataProcessing"]', false),
+      informationSciences: _.get(
+        form,
+        'input[name*="informationSciences"]',
+        false,
+      ),
+      mediaApplication: _.get(form, 'input[name*="mediaApplication"]', false),
+      noneApply: _.get(form, 'input[name*="noneApply"]', false),
+    };
+    return checkboxes;
+  };
 
   it('all checkboxes checked then noneApply checked causes all other checkboxes to be cleared', () => {
     const handleChange = sinon.spy();
-    // const form = ReactTestUtils.renderIntoDocument(
     const form = mount(
       <HighTechEmploymentTypeView
         formData={{
-          currentHighTechnologyEmployment: false,
-          pastHighTechnologyEmployment: true,
-          'view:salaryEmploymentTypes': {
-            highTechnologyEmploymentType: {
-              computerProgramming: true,
-              computerSoftware: true,
-              dataProcessing: true,
-              informationSciences: true,
-              mediaApplication: true,
-              noneApply: false,
-            },
-          },
+          computerProgramming: true,
+          computerSoftware: true,
+          dataProcessing: true,
+          informationSciences: true,
+          mediaApplication: true,
+          noneApply: false,
         }}
         onChange={handleChange}
       />,
     );
 
-    // const formDOM = getFormDOM(form);
+    const checkboxes = getCheckboxes(form);
 
-    // formDOM.click('input#noneApply[name="noneApply"]');
-    // formDOM.setCheckbox(
-    //   highTechnologyEmploymentTypes.computerProgramming,
-    //   true,
-    // );
-    // formDOM.click(highTechnologyEmploymentTypes.computerProgramming);
-
-    // const checkboxes = getCheckboxes(formDOM);
-    // formDOM.setCheckbox(highTechnologyEmploymentTypes.noneApply, true);
-    form.find(`input#noneApply`).simulate('change', {
-      target: { checked: true },
+    form.find(highTechnologyEmploymentTypes.noneApply).simulate('change', {
+      id: 'noneApply',
+      checked: true,
     });
-    // selectCheckbox(form, 'computerProgramming', true);
 
-    expect(form.find('input').length).to.equal(6);
-    // expect(form.find({ name: 'noneApply' }).checked).to.equal(true);
-    // expect(form.props('currentHighTechnologyEmployment')).to.equal(true);
-    // console.log(form.state());
+    expect(checkboxes.computerProgramming).to.equal(false);
+    expect(checkboxes.dataProcessing).to.equal(false);
+    expect(checkboxes.computerSoftware).to.equal(false);
+    expect(checkboxes.informationSciences).to.equal(false);
+    expect(checkboxes.mediaApplication).to.equal(false);
 
-    // const noneApplyCheckbox = form.find(
-    //   `input[name*="noneApply"]`
-    // );
+    form.unmount();
+  });
 
-    // formDOM.printTree();
-    // expect(checkboxes.noneApply.checked).to.equal(true);
-    // expect(_.get(form, 'input[name*="computerProgramming"]', false).length.to.equal(1));
-    // expect(form.find('input[name*="noneApply"]').value).to.equal(true);
-    // expect(noneApplyCheckbox.checked).to.equal(true);
-    // expect(noneApplyCheckbox.value).to.equal(true);
-    // expect(form.find(`input[name*="noneApply"]`).target).to.equal(false);
+  it('noneApply checkbox checked then any other checked causes noneApply checkbox to be cleared', () => {
+    const handleChange = sinon.spy();
+    const form = mount(
+      <HighTechEmploymentTypeView
+        formData={{
+          computerProgramming: false,
+          computerSoftware: false,
+          dataProcessing: false,
+          informationSciences: false,
+          mediaApplication: false,
+          noneApply: true,
+        }}
+        onChange={handleChange}
+      />,
+    );
 
-    // expect(checkboxes.computerProgramming.checked).to.equal(false);
-    // expect(checkboxes.computerSoftware.checked).to.equal(false);
-    // expect(checkboxes.dataProcessing.checked).to.equal(false);
-    // expect(checkboxes.informationSciences.checked).to.equal(false);
-    // expect(checkboxes.mediaApplication.checked).to.equal(false);
-    // expect(checkboxes.noneApply.checked).to.equal(true);
+    const checkboxes = getCheckboxes(form);
+
+    form
+      .find(highTechnologyEmploymentTypes.computerProgramming)
+      .simulate('change', {
+        id: 'computerProgramming',
+        checked: true,
+      });
+
+    expect(checkboxes.noneApply).to.equal(false);
 
     form.unmount();
   });
