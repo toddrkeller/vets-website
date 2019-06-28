ac6de1db23f2770073b49890dc489aa714eab42b
Dan Shawkey <shawkey_daniel@bah.com>
2019-06-28 08:18:19 -0400

    add searchAsDEA

--- src/applications/gi/tests/00-required.e2e.spec.js
+++ src/applications/gi/tests/00-required.e2e.spec.js
@@ -2,10 +2,10 @@ const E2eHelpers = require('../../../platform/testing/e2e/helpers');
 const Timeouts = require('../../../platform/testing/e2e/timeouts');
 const GiHelpers = require('./gibct-helpers');
 
-// const firstResult =
-//   '#react-root > div > div > div > div.search-page > div:nth-child(2) > div.search-results.small-12.usa-width-three-fourths.medium-9.columns.opened > div:nth-child(2) > div:nth-child(1) > div > div > div:nth-child(1) > div.small-12.usa-width-seven-twelfths.medium-7.columns > h2 > a';
-// const firstResultRate =
-//   '#react-root > div > div > div > div.search-page > div:nth-child(2) > div.search-results.small-12.usa-width-three-fourths.medium-9.columns.opened > div:nth-child(2) > div:nth-child(1) > div > div > div:nth-child(1) > div.small-12.usa-width-five-twelfths.medium-5.columns.estimated-benefits > div:nth-child(3) > div > h4 > div';
+const firstResult =
+  '#react-root > div > div > div > div.search-page > div:nth-child(2) > div.search-results.small-12.usa-width-three-fourths.medium-9.columns.opened > div:nth-child(2) > div:nth-child(1) > div > div > div:nth-child(1) > div.small-12.usa-width-seven-twelfths.medium-7.columns > h2 > a';
+const firstResultRate =
+  '#react-root > div > div > div > div.search-page > div:nth-child(2) > div.search-results.small-12.usa-width-three-fourths.medium-9.columns.opened > div:nth-child(2) > div:nth-child(1) > div > div > div:nth-child(1) > div.small-12.usa-width-five-twelfths.medium-5.columns.estimated-benefits > div:nth-child(3) > div > h4 > div';
 // const secondResult =
 //   '#react-root > div > div > div > div.search-page > div:nth-child(2) > div.search-results.small-12.usa-width-three-fourths.medium-9.columns.opened > div:nth-child(2) > div:nth-child(2) > div > div > div:nth-child(1) > div.small-12.usa-width-seven-twelfths.medium-7.columns > h2 > a';
 // const secondResultRate =
@@ -35,13 +35,13 @@ module.exports = E2eHelpers.createE2eTest(client => {
     .waitForElementVisible('.gi-app', Timeouts.slow)
     .axeCheck('.main');
 
-  // GiHelpers.searchAsDEA(
-  //   client,
-  //   firstResult,
-  //   firstResultRate,
-  //   GiHelpers.formatCurrency(GiHelpers.calculatorConstantsList.DEARATEOJT),
-  // );
-  //
+  GiHelpers.searchAsDEA(
+    client,
+    firstResult,
+    firstResultRate,
+    GiHelpers.formatCurrency(GiHelpers.calculatorConstantsList.DEARATEOJT),
+  );
+
   // // Loops through all "Enrolled" options for an ojt facility and verifies the DEA housing rate
   // for (let i = 2; i <= deaEnrolledMax; i += 2) {
   //   const deaRateOjtFormatted = Math.round(
