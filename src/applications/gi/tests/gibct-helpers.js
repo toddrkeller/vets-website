2d886fb6593640d11b8c17cb19f300f19b3d8315
Dan Shawkey <shawkey_daniel@bah.com>
2019-07-12 12:04:41 -0400

    add validateRates back in

--- src/applications/gi/tests/gibct-helpers.js
+++ src/applications/gi/tests/gibct-helpers.js
@@ -90,11 +90,13 @@ function searchAsDEA(client, expectedResult, resultRate, expectedRate) {
 // Verify the expected DEA housing rate for the selected "Enrolled" option
 function verifyDEA(client, enrolledOption, expectedResult) {
   selectProfilePage(client, expectedResult);
-  client.selectDropdown('enrolledOld', enrolledOption).waitForElementVisible(
-    housingRate,
-    Timeouts.normal,
-    // validateRates(client, housingRate, expectedDEA),
-  );
+  client
+    .selectDropdown('enrolledOld', enrolledOption)
+    .waitForElementVisible(
+      housingRate,
+      Timeouts.normal,
+      validateRates(client, housingRate, expectedResult),
+    );
 }
 
 const schools = {
