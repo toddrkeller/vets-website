3690a6d125504357ea4cae77f453a8f54604e548
Bill Tran <bill@billjohntran.com>
2019-07-30 23:05:56 -0700

    change host local to 0.0.0.0 to make it work for vagrant

--- src/site/constants/hostnames.js
+++ src/site/constants/hostnames.js
@@ -1,7 +1,7 @@
 const environments = require('./environments');
 
 module.exports = {
-  [environments.LOCALHOST]: 'localhost',
+  [environments.LOCALHOST]: '0.0.0.0',
   [environments.VAGOVDEV]: 'dev.va.gov',
   [environments.VAGOVSTAGING]: 'staging.va.gov',
   [environments.VAGOVPROD]: 'www.va.gov',
