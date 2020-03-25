d4cb5948259f84e2ad47813a8353a49cadb35e74
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-03-25 12:46:20 -0400

    Use commit hash as version.

--- script/publish-pact.js
+++ script/publish-pact.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 
+const { execSync } = require('child_process');
 const pact = require('@pact-foundation/pact-node');
 const path = require('path');
 
-const consumerVersion = `1.0.0-${process.env.BUILD_NUMBER ||
-  Math.floor(new Date() / 1000)}`;
+const consumerVersion = execSync('git rev-parse --verify HEAD')
+  .toString()
+  .trim();
 
 const opts = {
   pactFilesOrDirs: [path.resolve(__dirname, '../pacts')],
